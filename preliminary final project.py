1299575c054bd45a2165b6994d2193ceb161a243
Warren Zeng <96797919+warrenzeng15@users.noreply.github.com>
2023-11-15 20:20:04 -0500

    Update preliminary final project.py
    added a few more datasets that we can potentially use

--- preliminary final project.py
+++ preliminary final project.py
@@ -6,8 +6,21 @@ client = Socrata("data.ny.gov", app_token='geA6qNwvpHVBEDAVXq33qNDOb')
 print("")
 
 
+print("bikes in building")
 bikes_in_buildings = client.get("scjj-6yaf", select = 'tenantpostcode, noofbicyclerequested, requeststatus, council_district, census_tract, nta', limit = 50000) #not sure if all of these are necessary
 
 print(bikes_in_buildings)
 
+
+
+print("ramp lift usage")
+
+ramp_lift_usage = client.get("e2u6-bmnn", limit = 50000) 
+print (ramp_lift_usage)
+
+
+bus_service_delivered = client.get("2e6s-9gpm", limit = 50000) 
+
+
+mta_daily_ridership = client.get("vxuj-8kew", limit = 50000)
 
