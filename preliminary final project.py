b4eaadfb65536001b9a3cec0202692ffd3605d78
Warren Zeng <96797919+warrenzeng15@users.noreply.github.com>
2023-11-25 12:08:40 -0500

    Update preliminary final project.py

--- preliminary final project.py
+++ preliminary final project.py
@@ -1,15 +1,32 @@
-# preliminary final project
 import pandas as pd
+import operator
 from sodapy import Socrata
 
 client = Socrata("data.ny.gov", app_token='geA6qNwvpHVBEDAVXq33qNDOb')
 print("")
 
 
-print("bikes in building")
-bikes_in_buildings = client.get("scjj-6yaf", select = 'tenantpostcode, noofbicyclerequested, requeststatus, council_district, census_tract, nta', limit = 50000) #not sure if all of these are necessary
 
-print(bikes_in_buildings)
+bikes_in_buildings = client.get("scjj-6yaf", select = 'noofbicyclerequested, ownerzipcode', limit = 50000) #not sure if all of these are necessary
+
+#print(bikes_in_buildings)
+#find the areas with the most requests
+
+
+zip_codes = {}
+
+for entry in bikes_in_buildings:
+    if entry['ownerzipcode'] not in zip_codes:      #traverse through entire list of entries and add zip codes to dictionary while counting the entries for each zip code
+        zip_codes[entry['ownerzipcode']] = 1
+    else:
+        zip_codes[entry['ownerzipcode']] += 1
+
+# print(zip_codes) #this is a dictionary with the zip codes and the number of requests for each one
+sorted_zip_codes = dict( sorted(zip_codes.items(), key=operator.itemgetter(1), reverse=True)) #sorts by descending values. found the solution online: https://www.w3resource.com/python-exercises/dictionary/python-data-type-dictionary-exercise-1.php
+
+
+print('Dictionary in descending order by value : ', sorted_zip_codes)
+
 
 
 
